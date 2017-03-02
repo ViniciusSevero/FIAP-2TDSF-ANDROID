340647bb2b176bf5f9539369bcc4dd1f40d2d912
logonrm <logonrm@fiap.com.br>
2017-03-02 11:53:23 -0300

    Adicionado aula-03 (Activities, intents e splash screen)

---
+++ Aula03/app/src/main/java/com/example/vinicius/aula03/NotasCardActivity.java
@@ -0,0 +1,18 @@
+package com.example.vinicius.aula03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class NotasCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas_card);
+    }
+
+    public void voltar(View v){
+        finish();
+    }
+}
